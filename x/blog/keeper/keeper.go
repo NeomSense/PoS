60f071d66f0b4085a2ab888c4cd4f4383da74f10
Juan S <jsanjhern1992@gmail.com>
2025-10-03 00:05:01 -0400

    Commit PoS: update x/blog/keeper/keeper.go

--- x/blog/keeper/keeper.go
+++ x/blog/keeper/keeper.go
@@ -1,66 +1,50 @@
-package keeper
+\package keeper
 
 import (
-	"fmt"
-
-	"cosmossdk.io/collections"
-	"cosmossdk.io/core/address"
-	corestore "cosmossdk.io/core/store"
-
-	sdkcodec "github.com/cosmos/cosmos-sdk/codec"
-
-	"github.com/you/pos/x/blog/types"
+    "github.com/funmachine/pos/x/blog/types"
+    "github.com/cosmos/cosmos-sdk/codec"
+    "github.com/cosmos/cosmos-sdk/runtime"
+    sdk "github.com/cosmos/cosmos-sdk/types"
+    "cosmossdk.io/collections"
 )
 
+// Keeper defines the blog module's keeper
 type Keeper struct {
-	storeService corestore.KVStoreService
-	cdc          sdkcodec.Codec
-	addressCodec address.Codec
-
-	// Address capable of executing a MsgUpdateParams message (usually the gov module account).
-	authority string
-
-	Schema  collections.Schema
-	Params  collections.Item[types.Params]
-	PostSeq collections.Sequence
-	Post    collections.Map[uint64, types.Post]
+    cdc      codec.BinaryCodec
+    storeKey sdk.StoreKey
+    memStore sdk.KVStoreKey
+    // Collections for storing params, posts, and post count
+    Params    collections.Item[types.Params]
+    Posts     collections.Map[string, types.Post]
+    PostCount collections.Sequence
 }
 
+// NewKeeper creates a new blog keeper
 func NewKeeper(
-	storeService corestore.KVStoreService,
-	cdc sdkcodec.Codec,
-	addressCodec address.Codec,
-	authority string,
+    cdc codec.BinaryCodec,
+    storeKey sdk.StoreKey,
+    memStore sdk.KVStoreKey,
 ) Keeper {
-	// Validate authority is a proper bech32 address for the configured address codec.
-	if _, err := addressCodec.StringToBytes(authority); err != nil {
-		panic(fmt.Sprintf("invalid authority address %s: %s", authority, err))
-	}
-
-	sb := collections.NewSchemaBuilder(storeService)
-
-	k := Keeper{
-		storeService: storeService,
-		cdc:          cdc,
-		addressCodec: addressCodec,
-		authority:    authority,
-
-		// NOTE: CollValue is provided by github.com/cosmos/cosmos-sdk/codec in your setup.
-		Params:  collections.NewItem(sb, types.ParamsKey, "params", sdkcodec.CollValue[types.Params](cdc)),
-		Post:    collections.NewMap(sb, types.PostKey, "post", collections.Uint64Key, sdkcodec.CollValue[types.Post](cdc)),
-		PostSeq: collections.NewSequence(sb, types.PostCountKey, "postSequence"),
-	}
-
-	schema, err := sb.Build()
-	if err != nil {
-		panic(err)
-	}
-	k.Schema = schema
-
-	return k
-}
-
-// GetAuthority returns the module's authority (bech32 string).
-func (k Keeper) GetAuthority() string {
-	return k.authority
+    return Keeper{
+        cdc:      cdc,
+        storeKey: storeKey,
+        memStore: memStore,
+        Params: collections.NewItem(
+            runtime.NewKVStoreService(storeKey),
+            types.ParamsKey,
+            "params",
+            codec.CollValue[types.Params](cdc),
+        ),
+        Posts: collections.NewMap(
+            runtime.NewKVStoreService(storeKey),
+            types.PostKey,
+            "posts",
+            collections.StringKey,
+            codec.CollValue[types.Post](cdc),
+        ),
+        PostCount: collections.NewSequence(
+            runtime.NewKVStoreService(storeKey),
+            types.PostCountKey,
+        ),
+    }
 }
